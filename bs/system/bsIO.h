0379bbff9f3ca64c75e36e4748e2bee3a55eeae6
captainshadow <keiichisato82@gmail.com>
2014-03-26 01:40:59 +0900

    bsIO: add descriptive comments

--- bs/system/bsIO.h
+++ bs/system/bsIO.h
@@ -4,22 +4,66 @@
 
 + (void)onCreate;
 
-#pragma mark - asset
-+(NSString *)__assetPath:(NSString *)name;
-+(NSData *)assetG:(NSString *)name;
+#pragma mark - Asset
 
-#pragma mark - storage
-+(NSString *)__storagePath:(NSString *)name;
-+(BOOL)__checkStoragePath:(NSString *)name;
-+(NSData *)storageG:(NSString *)name;
-+(BOOL)storageS:(NSString *)name data:(NSData *)data;
-+(BOOL)storageD:(NSString *)name;
+/**
+ Returns the contents of specified file in the asset directory.
+ @param name The name of file to load.
+ @returns Returns data loaded from specified file.
+ @exception Throws bsException if  given file name is nil.
+ */
++ (NSData *)assetG:(NSString *)name;
 
-#pragma mark - cache
-+(NSString *)__cachePath:(NSString *)name;
-+(BOOL)__checkCachePath:(NSString *)name;
-+(NSData *)cacheG:(NSString *)name;
-+(BOOL)cacheS:(NSString *)name data:(NSData *)data;
-+(BOOL)cacheD:(NSString *)name;
+#pragma mark - Storage
+
+/**
+ Returns the contents of specified file in the document directory.
+ @param name The name of file to load.
+ @returns Returns data loaded from specified file.
+ @exception Throws bsException if given file name is nil.
+ */
++ (NSData *)storageG:(NSString *)name;
+
+/**
+ Creates a file in the document directory with given data and file name.
+ @param name The name of file to save.
+ @returns Returns YES if the file is saved successfully, otherwise NO.
+ @exception Throws bsException if given file name is nil.
+ */
++ (BOOL)storageS:(NSString *)name data:(NSData *)data;
+
+/**
+ Removes specified file from the document directory.
+ @param name The name of file to remove.
+ @returns Returns YES if the file is removed successfully, otherwise NO.
+ @exception Throws bsException if given file name is nil.
+ */
++ (BOOL)storageD:(NSString *)name;
+
+#pragma mark - Cache
+
+/**
+ Returns the contents of specified file in the cache directory.
+ @param name The name of file to load.
+ @returns Returns data loaded from specified file.
+ @exception Throws bsException if given file name is nil.
+ */
++ (NSData *)cacheG:(NSString *)name;
+
+/**
+ Creates a file in the cache directory with given data and file name.
+ @param name Destination file name.
+ @returns Returns YES if the file is saved successfully, otherwise NO.
+ @exception Throws bsException if given file name is nil.
+ */
++ (BOOL)cacheS:(NSString *)name data:(NSData *)data;
+
+/**
+ Removes specified file from the cache directory.
+ @param name The name of file to remove.
+ @returns Returns YES if the file is removed successfully, otherwise NO.
+ @exception Throws bsException if given file name is nil.
+ */
++ (BOOL)cacheD:(NSString *)name;
 
 @end
--- bs/system/bsIO.m
+++ bs/system/bsIO.m
@@ -11,63 +11,75 @@
 #import "bsMacro.h"
 #import "bsStr.h"
 
-static NSString* __bsIO_storagePath = nil;
-static NSString* __bsIO_assetPath = nil;
-static NSString* __bsIO_cachePath = nil;
+static NSString *__bsIO_storagePath = nil;
+static NSString * __bsIO_assetPath = nil;
+static NSString *__bsIO_cachePath = nil;
 static NSFileManager *__bsIO_fm = nil;
 
 @implementation bsIO
 
 + (id)alloc {
     
-    bsException( @"Static class 'bsIO' cannot be instantiated!" );
+    bsException(@"Static class 'bsIO' cannot be instantiated!");
     return nil;
 }
 
 + (id)allocWithZone:(NSZone *)zone {
     
-    bsException( @"Static class 'bsIO' cannot be instantiated!" );
+    bsException(@"Static class 'bsIO' cannot be instantiated!");
     return nil;
 }
 
 + (void)onCreate {
     
-    if( __bsIO_storagePath ) bsException(@"Wrong call");
-    __bsIO_storagePath = [NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES ) objectAtIndex:0];
+    if (__bsIO_storagePath) {
+        bsException(@"Wrong call");
+    }
+    __bsIO_storagePath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
     __bsIO_cachePath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0];
     __bsIO_assetPath = [NSString stringWithFormat:@"%@/assets", [[NSBundle mainBundle] bundlePath]];
     __bsIO_fm = [NSFileManager defaultManager];
-    if( ![__bsIO_fm fileExistsAtPath:__bsIO_storagePath] ) {
+    if (![__bsIO_fm fileExistsAtPath:__bsIO_storagePath]) {
         NSError *error = nil;
         [__bsIO_fm createDirectoryAtPath:__bsIO_storagePath withIntermediateDirectories:YES attributes:nil error:&error];
-        if( error ) {
-            bsException( @"A storage creation failded" );
+        if (error) {
+            bsException(@"A storage creation failded");
         }
     }
 }
 
 #pragma mark - asset
-+(NSString*)__assetPath:(NSString*)name {
+
++ (NSString *)__assetPath:(NSString *)name {
+    
     return [NSString stringWithFormat:@"%@/%@", __bsIO_assetPath, name];
 }
-+(NSData*)assetG:(NSString*)name {
-    if( name == nil ) bsException( @")A name is NULL");
+
++ (NSData *)assetG:(NSString *)name {
+    
+    if (name == nil) {
+        bsException( @")A name is NULL");
+    }
     NSString *path = [bsIO __assetPath:name];
     NSData *data = [__bsIO_fm contentsAtPath:path];
-    if ( data == nil ) {
-        bsException( @"A filename or a file path is wrong. name=%@", name );
+    if (data == nil) {
+        bsException(@"A filename or a file path is wrong. name=%@", name);
     }
     return data;
 }
 
 #pragma mark - storage
-+(NSString*)__storagePath:(NSString*)name {
+
++ (NSString *)__storagePath:(NSString *)name {
+    
     return [NSString stringWithFormat:@"%@/%@", __bsIO_storagePath, name];
 }
-+(BOOL)__checkStoragePath:(NSString*)name {
+
++ (BOOL)__checkStoragePath:(NSString *)name {
+    
     NSArray *names = [bsStr split:name seperator:@"/" trim:NO];
     BOOL success = YES;
-    if( [names count] > 1 ) {
+    if ([names count] > 1) {
         NSInteger i = [name length] - [names[[names count]-1] length];
         NSString *path = [bsIO __storagePath:[name substringToIndex:i]];
         NSFileManager *manager = [NSFileManager defaultManager];
@@ -77,65 +89,99 @@ static NSFileManager *__bsIO_fm = nil;
     }
     return success;
 }
-+(NSData*)storageG:(NSString*)name {
-    if( name == nil ) bsException( @"A name is NULL");
+
++ (NSData *)storageG:(NSString *)name {
+    
+    if (name == nil) {
+        bsException( @"A name is NULL");
+    }
     NSString *path = [bsIO __storagePath:name];
     NSData *data = [__bsIO_fm contentsAtPath:path];
     return data;
 }
-+(BOOL)storageS:(NSString*)name data:(NSData*)data {
-    if( name == nil ) bsException( @"A name is NULL");
-    if( data == nil ) {
+
++ (BOOL)storageS:(NSString *)name data:(NSData *)data {
+    
+    if (name == nil) {
+        bsException( @"A name is NULL");
+    }
+    if (data == nil) {
         return [self storageD:name];
     }
-    if( NO == [bsIO __checkStoragePath:name] ) return NO;
+    if (![bsIO __checkStoragePath:name]) {
+        return NO;
+    }
     NSString *path = [bsIO __storagePath:name];
     BOOL success = [__bsIO_fm createFileAtPath:path contents:data attributes:nil];
     return success;
 }
-+(BOOL)storageD:(NSString*)name {
-    if( name == nil ) bsException( @")A name is NULL");
+
++ (BOOL)storageD:(NSString *)name {
+    
+    if (name == nil) {
+        bsException( @")A name is NULL");
+    }
     NSString *path = [bsIO __storagePath:name];
     BOOL success = [__bsIO_fm removeItemAtPath:path error:nil];
     return success;
 }
 
 #pragma mark - cache
-+(NSString*)__cachePath:(NSString*)name {
+
++ (NSString *)__cachePath:(NSString *)name {
+    
     return [NSString stringWithFormat:@"%@/%@", __bsIO_cachePath, name];
 }
-+(BOOL)__checkCachePath:(NSString*)name {
+
++ (BOOL)__checkCachePath:(NSString *)name {
+    
     NSArray *names = [bsStr split:name seperator:@"/" trim:NO];
     BOOL success = YES;
-    if( [names count] > 1 ) {
+    if ([names count] > 1) {
         NSInteger i = [name length] - [names[[names count]-1] length];
         NSString *path = [bsIO __cachePath:[name substringToIndex:i]];
         NSFileManager *manager = [NSFileManager defaultManager];
-        if( ![manager fileExistsAtPath:path] ) {
+        if (![manager fileExistsAtPath:path]) {
             success = [manager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
         }
     }
     return success;
 }
-+(NSData*)cacheG:(NSString*)name {
-    if( name == nil ) bsException( @"A name is NULL");
+
++ (NSData *)cacheG:(NSString *)name {
+    
+    if (name == nil) {
+        bsException(@"A name is NULL");
+    }
     NSString *path = [bsIO __cachePath:name];
     NSData *data = nil;
-    if( [__bsIO_fm fileExistsAtPath:path] ) {
+    if ([__bsIO_fm fileExistsAtPath:path]) {
         data = [__bsIO_fm contentsAtPath:path];
     }
     return data;
 }
-+(BOOL)cacheS:(NSString*)name data:(NSData*)data {
-    if( name == nil ) bsException( @"A name is NULL");
-    if( data == nil ) bsException( @"A data is NULL");
-    if( NO == [bsIO __checkCachePath:name] ) return NO;
+
++ (BOOL)cacheS:(NSString *)name data:(NSData *)data {
+    
+    if (name == nil) {
+        bsException(@"A name is NULL");
+    }
+    if (data == nil) {
+        bsException(@"A data is NULL");
+    }
+    if (![bsIO __checkCachePath:name]) {
+        return NO;
+    }
     NSString *path = [bsIO __cachePath:name];
     BOOL success = [__bsIO_fm createFileAtPath:path contents:data attributes:nil];
     return success;
 }
-+(BOOL)cacheD:(NSString*)name {
-    if( name == nil ) bsException( @")A name is NULL");
+
++ (BOOL)cacheD:(NSString *)name {
+    
+    if (name == nil) {
+        bsException(@"A name is NULL");
+    }
     NSString *path = [bsIO __cachePath:name];
     BOOL success = [__bsIO_fm removeItemAtPath:path error:nil];
     return success;
